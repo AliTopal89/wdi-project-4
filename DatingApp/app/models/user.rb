c3e9c25630a7211b832a76090adf8ffef012ffe5
alitopaloglu <ali_t@gwmail.gwu.edu>
2015-06-01 01:22:17 -0400

    initialized models

--- DatingApp/app/models/user.rb
+++ DatingApp/app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ActiveRecord::Base
+	has_many :messages
+	has_many :pictures
+	has_many :likes
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
