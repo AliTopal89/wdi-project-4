72e4905c99a2f2879a995e960ddb4c116bfad24a
alitopaloglu <ali_t@gwmail.gwu.edu>
2016-01-15 16:01:34 -0500

    asset pipeline app.rb and pro.rb errors

--- DatingApp/config/application.rb
+++ DatingApp/config/application.rb
@@ -28,11 +28,11 @@ module DatingApp
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
-    config.serve_static_assets = false
+    config.serve_static_assets = true
     config.assets.compress = true
 
     # Don't fallback to assets pipeline if a precompiled asset is missed
-    config.assets.compile = false
+    config.assets.compile = true
 
     # Generate digests for assets URLs
     config.assets.digest = true
